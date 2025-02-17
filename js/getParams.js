7be458e96a79c7cc91894e386f7325b1c8346bab
ivan nguyen <ivan.h.nguyen@usace.army.mil>
2025-02-16 22:47:02 -0600

    got it to load a csh plot

--- js/getParams.js
+++
@@ -1,6 +0,0 @@
-function getUrlParameter(sParam) {
-    let url = new URL(window.location);
-    let test = url.searchParams.get(sParam);
-    console.log("getUrlParameter(" + sParam + ") = " + test);
-    return test;
-};
